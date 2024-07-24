4b790e310df2a975acde5aa46d9ca34b907caa41
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-07-24 10:56:29 +0300

    add form script

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,33 @@
-(() => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Открытие/закрытие меню
   const menuBtnRef = document.getElementById("open-menu");
   const closeBtnRef = document.getElementById("close-menu");
   const menu = document.querySelector(".backdrop-menu");
-
-  menuBtnRef.addEventListener("click", () => {
+  const openMenu = () => {
     menu.classList.remove("is-hidden");
-  });
-  closeBtnRef.addEventListener("click", () => {
+  };
+  const closeMenu = () => {
     menu.classList.add("is-hidden");
-  });
-  console.log("yes");
-})();
+  };
+  menuBtnRef.addEventListener("click", openMenu);
+  closeBtnRef.addEventListener("click", closeMenu);
+});
+
+//   Работа с формой
+const form = document.querySelector(".form-profile");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    phone: formData.get("phone"),
+  };
+
+  console.log("Form Data:", data);
+
+  // Очистка значений
+  form.reset();
+});
