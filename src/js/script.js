bd1a3aac76690e590613389c057e996458c54b58
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-07-24 18:00:11 +0300

    add script add/remove to favorites

--- src/js/script.js
+++ src/js/script.js
@@ -30,6 +30,34 @@ document.addEventListener("DOMContentLoaded", () => {
     // Очистка значений
     form.reset();
   });
+
+  //   Добавление в избранное
+  const galleryList = document.querySelector(".gallery__list");
+  const heartCountElement = document.querySelector(".heart-count");
+  let heartCount = 0;
+
+  function updateHeartCount() {
+    heartCountElement.textContent = heartCount;
+    if (heartCount > 0) {
+      heartCountElement.classList.remove("is-hidden");
+    } else {
+      heartCountElement.classList.add("is-hidden");
+    }
+  }
+
+  galleryList.addEventListener("click", function (event) {
+    if (event.target.closest(".card__btn")) {
+      const button = event.target.closest(".card__btn");
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        heartCount--;
+      } else {
+        button.classList.add("active");
+        heartCount++;
+      }
+      updateHeartCount();
+    }
+  });
 });
 
 window.onload = function () {
